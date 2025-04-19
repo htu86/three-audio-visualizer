2748d4ec291c77c5695392309fe341715b80cf0d
enrc <enrico.cortelazzi17@gmail.com>
2025-04-19 20:21:13 +0200

    Add: Rectangular light + fixed resolution in scene

--- src/render.ts
+++ src/render.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls, RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
 
 const sceneCanvas = document.getElementById("mainScene") as HTMLCanvasElement | null;
 if (!sceneCanvas) {
@@ -8,30 +8,39 @@ if (!sceneCanvas) {
 
 // Basic config
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas: sceneCanvas});
-const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 100 );
+const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const scene = new THREE.Scene();
 
 // Some extra renderer rules
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0x222222);
-renderer.shadowMap.enabled = true;
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.shadowMap.enabled = true;
 
-camera.position.set(0,5,30)
+camera.position.set(0,10,30)
 camera.translateX(38.4)
 
 // const controls = new OrbitControls(camera, renderer.domElement)
 
 
 // Light setup
-const light = new THREE.DirectionalLight(0xffffff,0.5)
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-light.position.set(-1, 2, 2)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.01);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+directionalLight.position.set(0, 1, 1); 
+directionalLight.translateX(38.4)
+
+const directionalLight2 = new THREE.RectAreaLight(0xffffff, 100.0,100,5);
+directionalLight2.position.set(38.4, 20, 0);
+directionalLight2.rotateX(-1.5708)
+scene.add(directionalLight,directionalLight2);
 
 // Adding the scene objects
-scene.add(light, ambientLight)
+scene.add(ambientLight,directionalLight)
 
 export function updateScene(){
-  // controls.update();
+  // controls.update(); 
   renderer.render(scene, camera);
 }
 
