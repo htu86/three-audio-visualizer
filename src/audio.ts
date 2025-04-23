ee8f8ef5f6fa60304fc974ab074a581a36432701
enrc <enrico.cortelazzi17@gmail.com>
2025-04-23 11:05:10 +0200

    Add: Functioning volume slider for some browsers

--- src/audio.ts
+++ src/audio.ts
@@ -1,9 +1,11 @@
 const fileInput = document.getElementById("fileSelect") as HTMLInputElement;
 const audioElement = document.getElementById("audioElement") as HTMLAudioElement;
+const volumeControl = document.getElementById("volumeControl") as HTMLInputElement
 
 let audioCtx: AudioContext;
 let audioSource: MediaElementAudioSourceNode;
 let audioAnalyser: AnalyserNode;
+let gainNode: GainNode;
 let bufferLength: number;
 let dataArray: Uint8Array;
 let barHeight: number;
@@ -24,23 +26,30 @@ audioElement?.addEventListener("play", () => {
     audioCtx = new AudioContext();
     audioSource = audioCtx.createMediaElementSource(audioElement);
     audioAnalyser = audioCtx.createAnalyser();
+    gainNode = audioCtx.createGain()
 
-    audioSource.connect(audioAnalyser);
+    audioSource.connect(gainNode);
+    gainNode.connect(audioAnalyser)
     audioAnalyser.connect(audioCtx.destination);
     audioAnalyser.fftSize = 128;
-
     bufferLength = audioAnalyser.frequencyBinCount;
     dataArray = new Uint8Array(bufferLength);
   }
-
   if (audioCtx.state === "suspended") {
     audioCtx.resume();
   }
 });
+
 audioElement?.addEventListener("pause", () => {
   if(!audioElement.paused){
     audioElement.pause()
   }
+})
+
+volumeControl?.addEventListener("input", (event: Event)=>{
+  const target = event.target as HTMLInputElement; // Type assertion for TypeScript
+  gainNode.gain.value = parseFloat(target.value)
+
 })
 
 export {
