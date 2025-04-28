c0db0abec0b88b3f9178590bd489eee3e3534716
enrc <enrico.cortelazzi17@gmail.com>
2025-04-28 09:09:01 +0200

    Add:Orbit controls, removed/renamed direct. lights

--- src/render.ts
+++ src/render.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const sceneCanvas = document.getElementById("mainScene") as HTMLCanvasElement | null;
 if (!sceneCanvas) {
@@ -7,35 +8,43 @@ if (!sceneCanvas) {
 
 // Basic config
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas: sceneCanvas});
-const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const scene = new THREE.Scene();
 
-// Some extra renderer rules
+
+// Some extra renderer rules for better resolution
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+//Camera config
 camera.position.set(0,10,30)
 camera.translateX(38.4)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target = new THREE.Vector3(38.4, 5 , 0);
+controls.enableDamping = true;
 
 // Light setup
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.01);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-directionalLight.position.set(0, 1, 1); 
-directionalLight.translateX(38.4)
+const rectLight = new THREE.RectAreaLight(0xffffff, 100.0,100,5);
+rectLight.position.set(38.4, 20, 0);
+rectLight.rotateX(-1.5708)
 
-const directionalLight2 = new THREE.RectAreaLight(0xffffff, 100.0,100,5);
-directionalLight2.position.set(38.4, 20, 0);
-directionalLight2.rotateX(-1.5708)
-scene.add(directionalLight,directionalLight2);
+window.addEventListener("resize", ()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 // Adding the scene objects
-scene.add(ambientLight,directionalLight)
+scene.add(ambientLight,rectLight)
 
+// Render the scene
 export function updateScene(){
   renderer.render(scene, camera);
+  controls.update()
 }
 
 export {renderer,camera,scene}
